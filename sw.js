b83bf3e82fb67f447abc942d2e4283a273d8ef8c
marops99 <linomarotta23@gmail.com>
2026-08-16 14:48:06 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,73 @@
+var CACHE_NAME = "persona-v18";
+
+var PRECACHE_URLS = [
+  "./index.html",
+  "./manifest.json",
+  "./js/core.js",
+  "./js/state.js",
+  "./js/home.js",
+  "./js/cibo.js",
+  "./js/salute.js",
+  "./js/diario.js",
+  "./js/soldi.js",
+  "./js/stats.js",
+  "./js/search.js",
+  "./js/app.js"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      // Cache each URL individually so one failure doesn't block install
+      return Promise.all(PRECACHE_URLS.map(function(url) {
+        return cache.add(url).catch(function() {});
+      }));
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      if (cached) {
+        fetch(event.request).then(function(response) {
+          if (response && response.status === 200) {
+            var clone = response.clone();
+            caches.open(CACHE_NAME).then(function(cache) {
+              cache.put(event.request, clone);
+            });
+          }
+        }).catch(function() {});
+        return cached;
+      }
+      return fetch(event.request).then(function(response) {
+        if (!response || response.status !== 200) return response;
+        var clone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, clone);
+        });
+        return response;
+      }).catch(function() {
+        return new Response("Offline", {status: 503, statusText: "Offline"});
+      });
+    })
+  );
+});
